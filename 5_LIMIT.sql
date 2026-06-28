de176da4761994f80c7ecef420c7214d78af4466
pavankumarreddyc08 <pavankumarreddyc08@gmail.com>
2026-06-28 22:56:40 +0530

    added LIMIT queries

---
+++ 5_LIMIT.sql
@@ -0,0 +1,36 @@
+select * 
+from customers
+limit 5;
+
+select price
+from products
+order by price desc
+limit 5;
+
+select price
+from products
+order by price asc
+limit 3;
+
+select * 
+from products
+limit 10;
+
+select * 
+from orders 
+order by order_date desc
+limit 5;
+
+select * 
+from suppliers
+limit 2;
+
+select * 
+from products
+order by stock_quantity desc
+limit 4;
+
+select * 
+from products
+order by price asc
+limit 7;
