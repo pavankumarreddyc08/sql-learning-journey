b77e3d48fd2a43ef61678c5bec73db33dd34aac1
pavankumarreddyc08 <pavankumarreddyc08@gmail.com>
2026-06-29 23:12:19 +0530

    added BETWEEN queries

---
+++ 7_BETWEEN.sql
@@ -0,0 +1,35 @@
+select * 
+from products
+where price between 1000 and 5000;
+
+select product_name, price
+from products
+where price between 5000 and 15000;
+
+select *
+from customers
+where customer_id between 105 and 112;
+
+select *
+from orders
+where order_date between '2025-05-10' and '2025-05-20';
+
+select *
+from products
+where stock_quantity between 20 and 60;
+
+select *
+from suppliers
+where supplier_id between 404 and 408;
+
+select *
+from products
+where price between 1500 and 3500;
+
+select *
+from orders
+where order_id between 305 and 315;
+
+select *
+from customers
+where registration_date between '2025-04-01' and '2025-05-01';
