7e4cf8f4b5d6a72d9144774a96627adcc24cf2a6
pavankumarreddyc08 <pavankumarreddyc08@gmail.com>
2026-06-29 22:21:06 +0530

    added DISTINCT queries

---
+++ 6_DISTINCT.sql
@@ -0,0 +1,28 @@
+use retailsphere_db;
+
+select distinct city
+from customers;
+
+select distinct city
+from suppliers;
+
+select distinct order_status
+from orders;
+
+select distinct category_id
+from categories;
+
+select distinct category_id
+from products;
+
+select distinct stock_quantity
+from products;
+
+select distinct price
+from products;
+
+select distinct registration_date
+from customers;
+
+select distinct category_name
+from categories;
